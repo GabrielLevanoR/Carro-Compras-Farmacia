3b29442a1cd3def925f16cbb8e8bdca9294742c0
LibniHuaranga <73641532@ucsm.edu.pe>
2022-05-23 12:13:27 -0500

    Codigo completo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Menu from './svg/bars-solid.svg'
-import Close from './svg/xmark-solid.svg'
-import CartIcon from './svg/cart-shopping-solid.svg'
-import { Link } from 'react-router-dom'
+import Close from './svg/times-solid.svg'
+import CartIcon from './svg/shopping-cart-solid.svg'
+import {Link} from 'react-router-dom'
 import './css/Header.css'
+import {DataContext} from './Context'
+
+
 
 export class Header extends Component {
+    static contextType = DataContext;
+
     state = {
         toggle: false
     }
+
     menuToggle = () =>{
         this.setState({toggle: !this.state.toggle})
     }
+
+
     render() {
         const {toggle} = this.state;
+        const {cart} = this.context;
         return (
             <header>
-                <div className='menu' onClick={this.menuToggle}>
-                    <img src={Menu} alt="" width="20"></img>
+                <div className="menu" onClick={this.menuToggle}>
+                    <img src={Menu} alt="" width="20"/>
                 </div>
-                <div className='logo'>
-                    <h1><Link to="/">Farmacia</Link></h1>
+                <div className="logo">
+                    <h1><Link to="/">FarmPeru</Link></h1>
                 </div>
                 <nav>
                     <ul className={toggle ? "toggle" : ""}>
-                        <li><Link to="/">Inicio</Link></li>
-                        <li><Link to="/product">Productos</Link></li>
-                        <li><Link to="/contact">Contato</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/login">Inicio Sesion / Registro</Link></li>
-                        <li className='close' onClick={this.menuToggle}>
-                            <img src={Close} alt="" width="20" />
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/product">Producto</Link></li>
+                        <li><Link to="/contact">Contactanos</Link></li>
+                        <li><Link to="/about">Sobre nosotros</Link></li>
+                        <li><Link to="/login">Login / Register</Link></li>
+                        <li className="close" onClick={this.menuToggle}>
+                            <img src={Close} alt="" width="20"/>
                         </li>
                     </ul>
-                    <div className='nav-cart'>
-                        <span>0</span>
+                    <div className="nav-cart">
+                        <span>{cart.length}</span>
                         <Link to="/cart">
-                            <img src={CartIcon} alt="" width="20" />
+                            <img src={CartIcon} alt="" width="20"/>
                         </Link>
                     </div>
                 </nav>
@@ -44,4 +53,5 @@ export class Header extends Component {
         )
     }
 }
+
 export default Header
